f2b0b6272a5961ca25151717db269b4cf6dee0c5
moha327 <m7amad9595@outlook.com>
2026-09-09 18:02:30 +0300

    Add CustomerServiceSimulation Problem

---
+++ CustomerServiceSimulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerServiceSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> customers = new Queue<string>();
+            customers.Enqueue("customer4");
+            customers.Enqueue("customer3");
+            customers.Enqueue("customer2");
+            customers.Enqueue("customer1");
+            Console.WriteLine("Serving customers:\n");
+            while (customers.Count > 0)
+            {
+                string currentCustomer = customers.Dequeue();
+                Console.WriteLine($"Serving: {currentCustomer}");
+            }
+        }
+    }
+}
