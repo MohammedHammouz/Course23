17890e68242b0c9f53ab5d2bd5bda21b7a61d1b3
moha327 <m7amad9595@outlook.com>
2026-09-09 18:55:24 +0300

    Add GenerateBinaryNumbers problem

---
+++ GenerateBinaryNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateBinaryNumbers
+{
+    internal class Program
+    {
+        static void GenerateBinaryNumbers(int n)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("1");
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string binary = queue.Dequeue();
+                Console.WriteLine(binary);
+                queue.Enqueue(binary + "0");
+                queue.Enqueue(binary + "1");
+            }
+        }
+        static void Main(string[] args)
+        {
+            GenerateBinaryNumbers(5);
+            Console.ReadKey();
+        }
+    }
+}
