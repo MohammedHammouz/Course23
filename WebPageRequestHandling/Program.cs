9ac1b90b67d066f324b70d40bfc63f80db827752
moha327 <m7amad9595@outlook.com>
2026-09-09 18:06:03 +0300

    Add WebPageRequestHandling problem

---
+++ WebPageRequestHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPageRequestHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> requests = new Queue<string>();
+            requests.Enqueue("Request3");
+            requests.Enqueue("Request2");
+            requests.Enqueue("Request1");
+            while (requests.Count > 0)
+            {
+                string currentRequest = requests.Dequeue();
+                Console.WriteLine($"Processed: {currentRequest}");
+            }
+        }
+    }
+}
