7087941aa2d3a455ae9139d60e9ca548d3b98146
moha327 <m7amad9595@outlook.com>
2026-09-09 18:30:37 +0300

    Add PalindromeQueue problem

---
+++ PalindromeQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeQueue
+{
+    internal class Program
+    {
+        static bool IsPalindrome(Queue<int> queue)
+        {
+            Stack<int> stack = new Stack<int>(queue);
+            foreach(var item in queue)
+            {
+                if (item != stack.Pop())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(new []{ 1, 2, 3, 2, 1 });
+            Console.WriteLine(IsPalindrome(queue));
+            Console.ReadKey();
+        }
+    }
+}
