ea0f0168b98cc75d0793774b8b6c08789382935b
moha327 <m7amad9595@outlook.com>
2026-09-09 17:58:21 +0300

    Add TaskSchedualing Problem

---
+++ TaskScheduling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskScheduling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> tasks = new Queue<string>();
+            tasks.Enqueue("task4");
+            tasks.Enqueue("task3");
+            tasks.Enqueue("task2");
+            tasks.Enqueue("task1");
+            while (tasks.Count > 0)
+            {
+                string currentTask = tasks.Dequeue();
+                Console.WriteLine($"Processing: {currentTask}");
+            }
+        }
+    }
+}
