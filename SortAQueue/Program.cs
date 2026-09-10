5b50bb358078680d2b0ed4b67021ad83227f03bf
moha327 <m7amad9595@outlook.com>
2026-09-10 15:25:49 +0300

    Add SortAQueue problem

---
+++ SortAQueue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAQueue
+{
+    internal class Program
+    {
+        static Queue<int>SortedAQueue(Queue<int> queue)
+        {
+            List<int> array = new List<int>(queue);
+            array.Sort();
+            return new Queue<int>(array);
+        }
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(new int[] { 2, 3, 1, 5, 4 });
+            Console.WriteLine(string.Join(",", SortedAQueue(queue)));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortAQueue/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sor AQueue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sor AQueue")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2024f1f-512d-49f9-b503-b42e7e0fbd30")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
