8e10800669b32aa3e71bb63df3aa22724f7779c2
moha327 <m7amad9595@outlook.com>
2026-09-10 19:43:37 +0300

    Add RotateAQueue problem

---
+++ RotateAQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateAQueue
+{
+    internal class Program
+    {
+        static Queue<int>RotatQueue(Queue<int> queue,int k)
+        {
+            k = k % queue.Count;
+           
+            while (k >=1)
+            {
+                Console.WriteLine(string.Join(",", queue));
+                queue.Enqueue(queue.Dequeue());
+                k--;
+            }
+            //{1,2,3,4,5}
+            //1   {4,5,1,2,3}
+            return queue;
+        }
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(new []{ 1, 2, 3, 4, 5});
+            Console.WriteLine(string.Join(",",(RotatQueue(queue, 17))));
+        }
+    }
+}
