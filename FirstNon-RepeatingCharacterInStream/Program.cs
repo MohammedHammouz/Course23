40a057e97571b550b58cfc67b597c1d4d349ae22
moha327 <m7amad9595@outlook.com>
2026-09-11 14:17:56 +0300

    Add FirstNon_RepeatingCharacterInStream problem

---
+++ FirstNon-RepeatingCharacterInStream/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstNon_RepeatingCharacterInStream
+{
+    
+    internal class Program
+    {
+        static void FindFirstRepeatingLetter(string stream)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            Queue<char> queue = new Queue<char>();
+            foreach (char item in stream)
+            {
+                if (!dict.ContainsKey(item))
+                    dict[item] = 0;
+                dict[item]++;
+                queue.Enqueue(item);
+                while(queue.Count>0 && dict[queue.Peek()] > 1)
+                {
+                    queue.Dequeue();
+                }
+                
+                Console.Write(queue.Count > 0 ? queue.Peek(): '-');
+                if (queue.Count > 0)
+                {
+                    queue.Dequeue();
+                }
+            }
+           
+            
+            
+        }
+        static void Main(string[] args)
+        {
+            FindFirstRepeatingLetter("aabbbbbbc");
+        }
+    }
+}
